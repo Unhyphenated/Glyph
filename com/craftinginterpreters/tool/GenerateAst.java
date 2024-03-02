180fe4338e168c412ff8944800de6bae0040c5d1
Julian Jong <julian.jong74@gmail.com>
2024-03-02 20:58:48 +0800

    Writer adds imports and classes according to the base name.

--- com/craftinginterpreters/tool/GenerateAst.java
+++ com/craftinginterpreters/tool/GenerateAst.java
@@ -11,6 +11,26 @@ public class GenerateAst {
             System.err.println("Usage: generate_ast <output directory>");
             System.exit(64);
         }
-        String outputDir = args[0];
+        String outputDirectory = args[0];
+        defineAst(outputDirectory, "Expr", Arrays.asList(
+            "Binary   : Expr left, Token operator, Expr right",
+            "Grouping : Expr expression",
+            "Literal  : Object value",
+            "Unary    : Token operator, Expr right"
+        ));
+    }
+
+    private static void defineAst(String outputDirectory, String baseClass, List<String> types) 
+    throws IOException{
+        String path = outputDirectory + "/" + baseClass + ".java";
+        PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+        writer.println("package.com/craftinginterpreters/lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseClass + " {");
+        writer.println("}");
+        writer.close();
     }
 }
