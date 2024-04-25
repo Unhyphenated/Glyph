02ac2e25c633050b3a3a7cc0698e49f0b4507076
Julian Jong <julian.jong74@gmail.com>
2024-04-25 21:19:14 +0800

    feat: Disable raw mode upon program exit

--- glyph.cpp
+++ glyph.cpp
@@ -1,15 +1,28 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
-void enableRawMode() {
-    struct termios raw;
-    tcgetattr(STDIN_FILENO, &raw);
+struct termios original_termios;
+void disableRawMode() {
+    tcsetattr(STDERR_FILENO, TCSAFLUSH, &original_termios);
+}
 
+void enableRawMode() {
+    // Get the terminal attributes of a standard terminal
+    tcgetattr(STDIN_FILENO, &original_termios);
+    atexit(disableRawMode);
+
+    // Create a reference to the original termios
+    struct termios raw = original_termios;
+
+    // Set the 'ECHO' bitflag to 0
     raw.c_lflag &= ~(ECHO);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
+    enableRawMode();
+
     char c;
     while (read(STDERR_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
