9164e5d1d920a1d190e0ddd08bb73a1bec3672e0
Julian Jong <julian.jong74@gmail.com>
2024-06-14 11:13:17 +0800

    add: Disable canonical mode for byte-by-byte reading

--- glyph.cpp
+++ glyph.cpp
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -15,8 +17,10 @@ void enableRawMode() {
     // Create a reference to the original termios
     struct termios raw = original_termios;
 
-    // Set the 'ECHO' bitflag to 0
-    raw.c_lflag &= ~(ECHO);
+    // Set the 'ECHO' bitflag to 0 
+        // ICANON is used to disable canonical mode allowing byte-by-byte
+        // reading instead of the standard line-by-line
+    raw.c_lflag &= ~(ECHO | ICANON);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -24,6 +28,14 @@ int main() {
     enableRawMode();
 
     char c;
-    while (read(STDERR_FILENO, &c, 1) == 1 && c != 'q');
+    // Quit terminal when 'q' is typed. Otherwise, display the the character
+    // and it's ASCII value
+    while (read(STDERR_FILENO, &c, 1) == 1 && c != 'q') {
+        if (!iscntrl(c)) {
+            printf("%d : %c\n", c, c);
+        } else {
+            printf("%d\n", c);
+        }
+    };
     return 0;
 }
