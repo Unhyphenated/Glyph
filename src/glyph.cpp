31ff56d1950af799091895d224ff83702dab3aa2
Julian Jong <julian.jong74@gmail.com>
2024-07-16 16:30:36 +0800

    add: Render tabs correctly

--- src/glyph.cpp
+++ src/glyph.cpp
@@ -14,6 +14,7 @@
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define GLYPH_VERSION "0.0.1"
+#define GLYPH_TAB_STOP 8
 
 enum cursorKeys {
     ARROW_UP = 1000,
@@ -29,12 +30,15 @@ enum cursorKeys {
 
 typedef struct erow {
     int size;
+    int rsize;
     char *chars;
+    char *render;
 } erow;
 
 /*** Data ***/
 struct editorConfig {
     int cx, cy;
+    int rx;
     int rowoff, coloff;
     int screenrows;
     int screencols;
@@ -204,12 +208,27 @@ void editorProcessKey() {
     }
 }
 
+int editorRowCxToRx(erow *row, int cx) {
+    int rx = 0;
+    int j;
+    for (j = 0; j < cx; j++) {
+        if (row -> chars[j] == '\t')
+            rx += (GLYPH_TAB_STOP - 1) - (rx % GLYPH_TAB_STOP);
+        rx++;
+    }
+    return rx;
+}
+
 /*** Output ***/
 void editorScroll() {
+    E.rx = 0;
+    if (E.cy < E.numrows) {
+        E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+    }
     if (E.cy < E.rowoff) E.rowoff = E.cy;
     if (E.cy >= E.rowoff + E.screenrows) E.rowoff = E.cy - E.screenrows + 1;
-    if (E.cx < E.coloff) E.coloff = E.cx;
-    if (E.cx >= E.coloff + E.screencols) E.coloff = E.cx - E.screencols + 1;
+    if (E.cx < E.coloff) E.coloff = E.rx;
+    if (E.cx >= E.coloff + E.screencols) E.coloff = E.rx - E.screencols + 1;
 }
 void editorDrawRows(Abuf& ab); // Initialise function that will be defined later (this causes an error is omitted)
 void editorRefreshScreen() {
@@ -220,7 +239,7 @@ void editorRefreshScreen() {
     editorDrawRows(AB);
     
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy - E.rowoff + 1, E.cx - E.coloff + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy - E.rowoff + 1, E.rx - E.coloff + 1);
     AB.append(buf, strlen(buf));
 
     AB.append("\x1b[?25h", 6); // Draws cursor
@@ -248,10 +267,10 @@ void editorDrawRows(Abuf& ab) {
                 ab.append("~", 1);
             }
         } else {
-            int len = E.row[filerow].size - E.coloff;
+            int len = E.row[filerow].rsize - E.coloff;
             if (len < 0) len = 0;
             if (len > E.screencols) len = E.screencols;
-            ab.append(&E.row[filerow].chars[E.coloff], len);
+            ab.append(&E.row[filerow].render[E.coloff], len);
         }
 
         ab.append("\x1b[K", 3);
@@ -290,6 +309,28 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
+void editorUpdateRow(erow *row) {
+    int tabs = 0;
+    int j;
+    for (j = 0; j < row -> size; j++)
+    if (row -> chars[j] == '\t') tabs++;
+
+    free(row -> render);
+    row->render = (char *)malloc(row -> size + (tabs * GLYPH_TAB_STOP - 1) + 1);
+    
+    int idx = 0;
+    for (j = 0; j < row -> size; j++) {
+        if (row -> chars[j] == '\t') {
+            row -> render[idx++] = ' ';
+            while (idx % GLYPH_TAB_STOP != 0) row -> render[idx++] = ' ';
+        } else {
+            row->render[idx++] = row->chars[j];
+        }
+    }
+    row->render[idx] = '\0';
+    row->rsize = idx;
+}
+
 void editorAppendRow(char *s, size_t len) {
     E.row = (erow* )realloc(E.row, sizeof(erow) * (E.numrows + 1));
     int at = E.numrows;
@@ -298,6 +339,10 @@ void editorAppendRow(char *s, size_t len) {
     E.row[at].chars = (char* )malloc(len + 1);
     memcpy(E.row[at].chars, s, len);
     E.row[at].chars[len] = '\0';
+
+    E.row[at].rsize = 0;
+    E.row[at].render = NULL;
+    editorUpdateRow(&E.row[at]);
     E.numrows++;
 }
 
@@ -320,6 +365,7 @@ void openEditor(char *filename) {
 void initEditor() {
     E.cx = 0;
     E.cy = 0;
+    E.rx = 0;
     E.numrows = 0;
     E.row = NULL;
     E.rowoff = 0;
