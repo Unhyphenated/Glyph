b197e9d2aaa719d251106e7c73eb5b59ae284d0f
Julian Jong <julian.jong74@gmail.com>
2024-07-16 11:32:11 +0800

    add: Error handling if cursor goes out of bounds

--- src/glyph.cpp
+++ src/glyph.cpp
@@ -101,16 +101,24 @@ int editorReadKey() {
 void editorMoveCursor(int key) {
     switch (key) {
         case ARROW_UP:
+        if (E.cy != 0) {
             E.cy--;
+        }
             break;
         case ARROW_LEFT:
+        if (E.cx != 0) {
             E.cx--;
+        }
             break;
         case ARROW_DOWN:
+        if (E.cy == E.screenrows - 1) {
             E.cy++;
+        }
             break;
         case ARROW_RIGHT:
+        if (E.cx == E.screencols - 1) {
             E.cx++;
+        }
             break;
     }
 }
