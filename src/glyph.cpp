6c344b8c9353d4170fa9012c5d7f7481a543d587
Julian Jong <julian.jong74@gmail.com>
2024-07-16 15:49:18 +0800

    add: Move cursor when at the beginning and end of lines

--- src/glyph.cpp
+++ src/glyph.cpp
@@ -134,26 +134,37 @@ int editorReadKey() {
 }
 
 void editorMoveCursor(int key) {
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
     switch (key) {
         case ARROW_UP:
-        if (E.cy != 0) {
-            E.cy--;
-        }
+        if (E.cy != 0) E.cy--;
             break;
+
         case ARROW_LEFT:
         if (E.cx != 0) {
             E.cx--;
+        } else if (E.cy > 0) {
+            E.cy--;
+            E.cx = E.row[E.cy].size;
         }
             break;
+
         case ARROW_DOWN:
-        if (E.cy < E.numrows) {
-            E.cy++;
-        }
+        if (E.cy < E.numrows) E.cy++;
             break;
+
         case ARROW_RIGHT:
+        if (row && E.cx < row -> size) {
             E.cx++;
+        } else if (row && E.cx == row -> size) {
+            E.cy++;
+            E.cx = 0;
+        }
             break;
     }
+    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    int rowlen = row ? row -> size : 0;
+    if (E.cx > rowlen) E.cx = rowlen;
 }
 
 void editorProcessKey() {
